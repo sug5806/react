8b5087199499c29586727c3e5339c7191263b39d
홍동수 <dsh@storecamera.io>
2020-05-31 13:51:34 +0900

    완성

--- src/index.js
+++ src/index.js
@@ -1,56 +1,8 @@
 import React from 'react';
-
-class CardContainer extends React.Component {
-    constructor(props){
-        // 1. 부모 컴포넌트로 props 전달
-        super(props);
-        // 2. 컴포넌트의 state 객체 초기화
-        this.state = {
-            card: []
-        };
-
-        this.setState({
-            cards : [{
-                "id": 1,
-                "img": "img/strings.png",
-                "imgalt": "strings",
-                "desc": "A very authentic and beautiful instrument!!",
-                "price": 100.0,
-                "productname": "Strings"
-            }, {
-                "id": 2,
-                "img": "img/redguitar.jpeg",
-                "imgalt": "redg",
-                "desc": "A really cool red guitar that can produce super cool music!!",
-                "price": 299.0,
-                "productname": "RedGuitar"
-            }]
-        })
-    }
-
-    render() {
-        const cards = this.state.cards;
-        let items = cards.map(
-            card => <Card key={card.id} {...card}/>
-        );
-        return (
-            <div className = 'container pt-4'>
-                <h3 className='text-center text-primary'>Products</h3>
-                <div className="pt-4 row">
-                    {items}
-                </div>
-            </div>
-        );
-    }
-}
+import ReactDOM from 'react-dom';
 
 class Card extends React.Component {
     render() {
-        const img = "img/strings.png";
-        const imgalt = "string";
-        const desc = "A very authentic and beautiful instrument!!";
-        const price = 100;
-        const productName = "Strings";
         return (
             <div className="col-md-6 col-lg-4 d-flex align-items-stretch">
                 <div className="card mb-3">
@@ -67,8 +19,46 @@ class Card extends React.Component {
     }
 }
 
+class CardContainer extends React.Component {
+    constructor(props) {
+        // 1. 부모 컴포넌트로 props 전달
+        super(props);
+        // 2. 컴포넌트의 state 객체 초기화
+        this.state = {
+            cards: []
+        };
+    }
+
+    componentDidMount() {
+        console.log('Component Did Mount Called: ' + new Date().toLocaleString());
+        fetch('cards.json')
+            .then(res => res.json())
+            .then((result) => {
+                console.log('Fetch...');
+                this.setState({
+                    cards: result
+                });
+            });
+    }
+
+    render() {
+        const cards = this.state.cards;
+        let items = cards.map(
+            card => <Card key={card.id} {...card} />
+        );
+        return (
+            <div className='container pt-4'>
+                <h3 className='text-center text-primary'>Products</h3>
+                <div className="pt-4 row">
+                    {items}
+                </div>
+            </div>
+        );
+    }
+}
+
 
 ReactDOM.render(
     <CardContainer />,
     document.getElementById('root')
-)
+);
