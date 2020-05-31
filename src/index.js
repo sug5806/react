13b8ec2cfa6c283cbf0a3804cd51f0aaa575bcea
홍동수 <dsh@storecamera.io>
2020-05-31 13:24:50 +0900

    map method 사용

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
-class CardContainer extends React.Component{
+class CardContainer extends React.Component {
     render() {
-        return(
-            <div>
-                <Card key='1' img="img/strings.png" alt="strings" productName="Strings" price='100.0' desc="A very authentic and beautiful instrument!!" />
-                <Card key='2' img="img/redguitar.jpeg" alt="redg" productName="RedGuitar" price='299.0' desc="A really cool red guitar that can produce super cool music!!" />
-            </div>
+        const cards = [{
+            "id": 1,
+            "img": "img/strings.png",
+            "imgalt": "strings",
+            "desc": "A very authentic and beautiful instrument!!",
+            "price": 100.0,
+            "productname": "Strings"
+        }, {
+            "id": 2,
+            "img": "img/redguitar.jpeg",
+            "imgalt": "redg",
+            "desc": "A really cool red guitar that can produce super cool music!!",
+            "price": 299.0,
+            "productname": "RedGuitar"
+        }];
+
+        const cardItems = cards.map(
+            card => <Card key={card.id} img={card.img} alt={card.imgalt} productName={card.productname} price={card.price} desc={card.desc} />
+        );
+        return (
+            <div>{cardItems}</div>
         );
     }
 }
