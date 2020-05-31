66d248a5e779bffc3487303e738f5bfbbf0a1532
홍동수 <dsh@storecamera.io>
2020-05-31 13:32:17 +0900

    state 추가

--- src/index.js
+++ src/index.js
@@ -1,28 +1,45 @@
 import React from 'react';
 
 class CardContainer extends React.Component {
-    render() {
-        const cards = [{
-            "id": 1,
-            "img": "img/strings.png",
-            "imgalt": "strings",
-            "desc": "A very authentic and beautiful instrument!!",
-            "price": 100.0,
-            "productname": "Strings"
-        }, {
-            "id": 2,
-            "img": "img/redguitar.jpeg",
-            "imgalt": "redg",
-            "desc": "A really cool red guitar that can produce super cool music!!",
-            "price": 299.0,
-            "productname": "RedGuitar"
-        }];
+    constructor(props){
+        // 1. 부모 컴포넌트로 props 전달
+        super(props);
+        // 2. 컴포넌트의 state 객체 초기화
+        this.state = {
+            card: []
+        };
 
-        const cardItems = cards.map(
-            card => <Card key = {card.id} {...card}/>
+        this.setState({
+            cards : [{
+                "id": 1,
+                "img": "img/strings.png",
+                "imgalt": "strings",
+                "desc": "A very authentic and beautiful instrument!!",
+                "price": 100.0,
+                "productname": "Strings"
+            }, {
+                "id": 2,
+                "img": "img/redguitar.jpeg",
+                "imgalt": "redg",
+                "desc": "A really cool red guitar that can produce super cool music!!",
+                "price": 299.0,
+                "productname": "RedGuitar"
+            }]
+        })
+    }
+
+    render() {
+        const cards = this.state.cards;
+        let items = cards.map(
+            card => <Card key={card.id} {...card}/>
         );
         return (
-            <div>{cardItems}</div>
+            <div className = 'container pt-4'>
+                <h3 className='text-center text-primary'>Products</h3>
+                <div className="pt-4 row">
+                    {items}
+                </div>
+            </div>
         );
     }
 }
@@ -50,3 +67,8 @@ class Card extends React.Component {
     }
 }
 
+
+ReactDOM.render(
+    <CardContainer />,
+    document.getElementById('root')
+)
